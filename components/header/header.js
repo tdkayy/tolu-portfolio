8a186d2a55083474d08fd28864a7382486f06d8b
tdkayy <toluadesanya@yahoo.co.uk>
2025-06-27 21:11:04 +0100

    newheader

--- components/header/header.js
+++ components/header/header.js
@@ -1,13 +1,21 @@
+import Link from 'next/link'
+
 export default function Header() {
-    return (
-      <header className="py-4 px-6 bg-white shadow-md">
-        <nav className="flex justify-between items-center max-w-5xl mx-auto">
-          <a href="/" className="text-xl font-bold">[YourName].dev</a>
-          <a href="https://github.com/tdkayy" target="_blank" rel="noopener noreferrer" className="text-sm">
-            GitHub
-          </a>
-        </nav>
-      </header>
-    )
-  }
-  
+  return (
+    <header className="py-4 px-6 bg-white shadow-md">
+      <nav className="flex justify-between items-center max-w-5xl mx-auto">
+        <Link href="/" className="text-xl font-bold">
+          [Tolu ADESANYA].dev
+        </Link>
+        <a
+          href="https://github.com/tdkayy"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-sm"
+        >
+          GitHub
+        </a>
+      </nav>
+    </header>
+  )
+}
