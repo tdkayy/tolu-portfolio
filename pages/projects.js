7b1e872d6efc710d130232174de993af023899f0
tdkayy <toluadesanya@yahoo.co.uk>
2026-07-12 19:55:36 +0100

    new

--- pages/projects.js
+++ pages/projects.js
@@ -4,32 +4,38 @@ import ProjectList from '../components/projects/projectList'
 import Link from 'next/link'
 
 export default function Projects() {
-  return (
-    <>
-      <Header />
-      <main className="max-w-5xl mx-auto px-4 mt-12">
-      <h1 className="text-3xl font-bold text-center">who are you!!</h1>
-        <p className="mt-3 text-lg opacity-90">
-        I would describe myself as a thinker focused on building tools that are scalable, human-centred, and purpose-driven. My long-term mission is to develop the infrastructure that empowers communities, starting here in London, and one day reaching my home country, Nigeria.
-        </p>
+    return ( <
+        >
+        <
+        Header / >
+        <
+        main className = "max-w-5xl mx-auto px-4 mt-12" >
+        <
+        h1 className = "text-3xl font-bold text-center" > about me < /h1> <
+        p className = "mt-3 text-lg opacity-90" >
+        i am a junior systems engineer who learns by building and making mistakes.i have been focusing on backend engineering, in the form of low latency architecture using Rust, delving into using and understanding event - driven APIs, optimizing memory constraints
+        for embedded hardware etc. <
+        /p>
 
-        <div className="mt-10">
-          <ProjectList />
-        </div>
+        <
+        div className = "mt-10" >
+        <
+        ProjectList / >
+        <
+        /div>
 
-        {/* home bottom */}
-        <div className="mt-8 text-center">
-          <Link
-            href="/"
-            className="px-5 py-3 rounded-md border border-gray-300 hover:bg-gray-100 
-            dark:border-gray-700 dark:hover:bg-gray-800 
-            transition text-sm font-medium"
-          >
-            ← Home
-          </Link>
-        </div>
-      </main>
-      <Footer />
-    </>
-  )
+        { /* home bottom */ } <
+        div className = "mt-8 text-center" >
+        <
+        Link href = "/"
+        className = "px-5 py-3 rounded-md border border-gray-300 hover:bg-gray-100 
+        dark: border - gray - 700 dark: hover: bg - gray - 800 transition text - sm font - medium " >
+        ←Home <
+        /Link> <
+        /div> <
+        /main> <
+        Footer / >
+        <
+        />
+    )
 }
